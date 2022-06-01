d552538a6383c3c1b9a0d63b9f7787d74b3ef389
Maziar <taghipour.maziar@gmail.com>
2022-06-01 08:00:51 -0700

    Trimming

--- src/main/java/com/mvp/mvpbackendchallenge/security/SecurityConfig.java
+++ src/main/java/com/mvp/mvpbackendchallenge/security/SecurityConfig.java
@@ -1,13 +1,11 @@
 package com.mvp.mvpbackendchallenge.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,8 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
-	@Autowired
-	private CustomAuthenticationProvider customAuthenticationProvider;
+	private final CustomAuthenticationProvider customAuthenticationProvider;
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
@@ -47,26 +44,4 @@ public class SecurityConfig {
 		http.headers().frameOptions().disable();
 		return http.build();
 	}
-
-   //	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http
-//				.httpBasic()
-//				.and()
-//				.csrf()
-//				.disable()
-//				.authenticationProvider(customAuthenticationProvider)
-//				.authorizeRequests()
-//				.antMatchers("/mvp/vending/user/register")
-//				.permitAll()
-//				.and()
-//				.authorizeRequests()
-//				.antMatchers("/h2-console/**")
-//				.permitAll()
-//				.and()
-//				.authorizeRequests()
-//				.anyRequest().authenticated();
-//
-//		http.headers().frameOptions().disable();
-//	}
 }
